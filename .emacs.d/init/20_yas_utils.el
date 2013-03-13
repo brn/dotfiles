6145144e1913ecdc36260d3c2beb4a0f4f1a06ee
brn <dobaw20@gmail.com>
2013-03-13 10:55:59 +0900

    fix makefile.

--- .emacs.d/init/20_yas_utils.el
+++ .emacs.d/init/20_yas_utils.el
@@ -1,3 +1,45 @@
+;;camp.moduleのパス自動補完機能
+(defun yas-util-get-module-path()
+  (let*
+      (
+       ;;modules/で分割する
+       (dir-path (car (cdr (split-string default-directory "modules/"))))
+       (ret "")
+       path-arr)
+    (defun replace-cases(ch-num next-ch-num list result-str skip?)
+      (let*
+          (
+           (ch-str (char-to-string ch-num))
+           next-ch-str
+           appended
+           skip-next?)
+        (if (not skip?)
+            (progn
+              (if next-ch-num (setq next-ch-str (char-to-string next-ch-num)))
+              (if next-ch-str
+                  (if (equal ch-str "/")
+                      (setq appended (concat result-str "."))
+                    (if (equal ch-str "_")
+                        (progn
+                          (setq appended (concat result-str (upcase next-ch-str)))
+                          (setq skip-next? t))
+                      (setq appended (concat result-str ch-str))))
+                ;;最後が/の場合破棄する
+                (if (equal ch-str "/")
+                    (setq appended result-str)
+                  (setq appended (concat result-str ch-str)))))
+          (setq appended result-str))
+        (if list
+            (replace-cases (car list) (car (cdr list)) (cdr list) appended skip-next?)
+          appended)))
+    (if dir-path
+        (progn
+          (setq path-arr (string-to-list dir-path))
+          (replace-cases (car path-arr) (car (cdr path-arr)) (cdr path-arr) ret nil))
+      ret)))
+
+
+;;アッパーキャメルからローワーキャメルに変換
 (defun yas-util-upper-to-lower-camel(text)
   (if (/= (length text) 0)
       (concat
@@ -8,3 +50,8 @@
         (cdr (string-to-list text))
         'string))
     text))
+
+
+;;パスからクラス名を取得
+(defun yas-util-get-class-from-module(text sep)
+  (if (string-match (concat "\\" sep) text) (car (last (split-string text (concat "\\" sep)))) text))
