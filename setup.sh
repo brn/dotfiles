27e54b23f51315059eab09bdd69c091b2499b833
brn <dobaw20@gmail.com>
2015-01-20 23:36:24 +0900

    UPDATED.

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ brew bundle
 mv '/opt/homebrew-cask/Caskroom/google-chrome/latest/Google Chrome.app' /Applications/
 
 git submodule update --init
+cd ./.emacs.d/ && grunt
+cd ../
 
 ln -s ./zsh/.zshrc ~/.zshrc
 ln -s ./zsh/.zshrc.local ~/.zshrc.local
 ln -s ./zsh/.zshrc.osx ~/.zshrc.osx
 ln -s ./tmux/.tmux.conf ~/.tmux.conf
-ln -s ./.emacs ~/
+ln -s ./.emacs.d ~/
 
 cd karabiner && sh setting.sh 
